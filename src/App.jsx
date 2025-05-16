463a1f8fe2c1fab7edb55c3091f3ff009c35dd69
VanThuat02 <thuatnguyenvan262005@gmail.com>
2025-05-16 14:48:16 +0700

    group web gaming

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Characters from "./components/Characters";
+
+
+
+const App = () => {
+  return (
+    <div className=''>
+      <Header/>
+      <Hero />
+      <Characters/>
+    </div>
+  )
+}
+
+export default App
